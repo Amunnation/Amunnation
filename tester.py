963cbcc215d43ac21f61fce4c1b0430fcc6e4484
Amunnation <85100610+Amunnation@users.noreply.github.com>
2021-05-31 17:35:10 +0200

    main.py

--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
                                              
     hosts_file = open('dns','r')
     no = 0                                     
-    valid = "Cloudflare"
+    valid = "aws"
     user_agent = {
 	"User-Agent" : "Browser: Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36"
     }
@@ -24,10 +24,11 @@ if __name__ == '__main__':
         host = "http://check-host.net/ip-info?host=" +host
         response = requests.get(host,headers=user_agent,verify=False)
         if(valid in response.text):                     
-            print(f'[-] Checking [{no}] {host2} ->Valid Host!! {valid} Found In Response')
+            print(f'[-] Checking [{no}] {host2} ->Valid Host!! Cloudfront Found In Response')
                                                                 
-            with open("hoster.txt","a") as mano:
+            with open("scraped.txt","a") as mano:
                 mano.write(host2+"\n")
         else:                   
             print(f'[-] Checking [{no}] {host2} ->Invalid Host!!')                                                                                            
-    print("\nPOSSIBLE HOSTS HAVE BEEN WRITTEN TO hoster.txt  ")
+    print("\nPOSSIBLE HOSTS HAVE BEEN WRITTEN TO scraped.txt ")
+    print("\nCOMMANDS \n+ nano scraped.txt TO SEE THEM")
